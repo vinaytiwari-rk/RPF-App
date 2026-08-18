9120d54ab479b02419a85e2665d991a4aaaf7114
vinaytiwari-rk <tiwari.vinay89@gmail.com>
2026-08-18 11:57:04 +0530

    Register native embedded browser plugin

--- android/app/src/main/java/com/rpfoundation/app/MainActivity.java
+++ android/app/src/main/java/com/rpfoundation/app/MainActivity.java
@@ -1,5 +1,11 @@
 package com.rpfoundation.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity { }
+public class MainActivity extends BridgeActivity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(NativeBrowserPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
