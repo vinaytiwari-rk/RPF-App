c6672a4d5d7263b104c56c0b1b2c419080dca49f
vinaytiwari-rk <tiwari.vinay89@gmail.com>
2026-08-23 17:10:22 +0530

    chore(phase-1): register governed content route in route patcher

--- addRoutes.cjs
+++ addRoutes.cjs
@@ -4,17 +4,17 @@ const path = require('path');
 const serverPath = path.join(__dirname, 'server.ts');
 let content = fs.readFileSync(serverPath, 'utf8');
 
-// import
-content = content.replace(
-  'import adminHqExtraRoutes from \\'./src/routes/adminHqExtraRoutes.js\\';',
-  'import adminHqExtraRoutes from \\'./src/routes/adminHqExtraRoutes.js\\';\nimport adminDynamicRoutes from \\'./src/routes/adminDynamicRoutes.js\\';'
-);
+function addImport(importLine, after) {
+  if (!content.includes(importLine)) content = content.replace(after, `${after}\n${importLine}`);
+}
+function addUse(useLine, after) {
+  if (!content.includes(useLine)) content = content.replace(after, `${after}\n${useLine}`);
+}
 
-// use
-content = content.replace(
-  'app.use(adminHqExtraRoutes);',
-  'app.use(adminHqExtraRoutes);\napp.use(adminDynamicRoutes);'
-);
+addImport("import adminDynamicRoutes from './src/routes/adminDynamicRoutes.js';", "import adminHqExtraRoutes from './src/routes/adminHqExtraRoutes.js';");
+addImport("import governedContentRoutes from './src/routes/governedContentRoutes.js';", "import adminDynamicRoutes from './src/routes/adminDynamicRoutes.js';");
+addUse('app.use(adminDynamicRoutes);', 'app.use(adminHqExtraRoutes);');
+addUse('app.use(governedContentRoutes);', 'app.use(adminDynamicRoutes);');
 
 fs.writeFileSync(serverPath, content, 'utf8');
 console.log('Routes added');
