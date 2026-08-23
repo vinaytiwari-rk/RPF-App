b9e5067cf730f90d2e024a5250e084487583ac26
vinaytiwari-rk <tiwari.vinay89@gmail.com>
2026-08-23 17:27:56 +0530

    fix(phase-1): register metrics route through route patcher

--- addRoutes.cjs
+++ addRoutes.cjs
@@ -13,8 +13,10 @@ function addUse(useLine, after) {
 
 addImport("import adminDynamicRoutes from './src/routes/adminDynamicRoutes.js';", "import adminHqExtraRoutes from './src/routes/adminHqExtraRoutes.js';");
 addImport("import governedContentRoutes from './src/routes/governedContentRoutes.js';", "import adminDynamicRoutes from './src/routes/adminDynamicRoutes.js';");
+addImport("import metricsRoutes from './src/routes/metricsRoutes.js';", "import governedContentRoutes from './src/routes/governedContentRoutes.js';");
 addUse('app.use(adminDynamicRoutes);', 'app.use(adminHqExtraRoutes);');
 addUse('app.use(governedContentRoutes);', 'app.use(adminDynamicRoutes);');
+addUse('app.use(metricsRoutes);', 'app.use(governedContentRoutes);');
 
 fs.writeFileSync(serverPath, content, 'utf8');
 console.log('Routes added');
